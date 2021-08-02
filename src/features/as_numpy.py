e604429692755d8a35bcce41f8b9e5a78d5dd945
Murl Easley <murl@easley-family.com>
2021-08-01 21:59:47 -0700

    Corrected minor formatting errors - long lines, etc.

--- src/features/as_numpy.py
+++ src/features/as_numpy.py
@@ -7,17 +7,15 @@ def get_data_as_numpy_array(clean_data_file_path, num_columns):
         rows = f.readlines()
         for row_num in range(len(rows)):
             try:
-                row = np.array([rows[row_num].rstrip("\n").split("\t")], dtype=float)
+                row = np.array([rows[row_num].rstrip("\n").split("\t")],
+                               dtype=float)
             except ValueError:
-                raise ValueError("Line {0} of {1} is badly formatted".format(row_num + 1, clean_data_file_path))
+                raise ValueError("Line {0} of {1} is badly formatted".format(
+                    row_num + 1, clean_data_file_path))
             else:
                 if row.shape != (1, num_columns):
-                    raise ValueError("Line {0} of {1} does not have {2} columns".format(row_num + 1,
-                                                                                        clean_data_file_path,
-                                                                                        num_columns
-                                                                                        )
-                                     )
+                    raise ValueError(
+                        "Line {0} of {1} does not have {2} columns".format(
+                            row_num + 1, clean_data_file_path, num_columns))
             result = np.append(result, row, axis=0)
     return result
-
-
